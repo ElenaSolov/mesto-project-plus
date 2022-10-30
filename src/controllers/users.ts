de78c0d629ebdcc75ffaac35e78cc7884b471d37
Elena Soloveva <seaver@yandex.ru>
2022-10-30 15:39:27 +0200

    feat: add update user profile and avatar functions and routes

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -14,8 +14,15 @@ import User from '../models/user';
 //     .then((newUser) => res.status(201).send({ name: newUser.name, about: newUser.about }))
 //     .catch((err) => res.status(400).send(err));
 // };
+export const getUsers = async (req: Request, res: Response) => {
+  await User.find({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => console.log(err));
+};
 
-const createUser = async (req: Request, res: Response) => {
+export const createUser = async (req: Request, res: Response) => {
   await User.create({
     name: req.body.name,
     about: req.body.about,
@@ -25,4 +32,37 @@ const createUser = async (req: Request, res: Response) => {
     .catch((err) => res.status(400).send(err));
 };
 
-export default createUser;
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await User.findById(id).exec()
+    .then((user) => res.status(200).send(user))
+    .catch(() => res.status(404).send({ error: 'Пользователь не найден' }));
+};
+
+export const updateUserProfile = async (req: Request, res: Response) => {
+  // @ts-ignore
+  const id = req.user._id;
+
+  await User.findByIdAndUpdate(id, {
+    name: req.body.name,
+    about: req.body.about,
+    avatar: req.body.avatar,
+  }, {
+    new: true,
+  }).exec()
+    .then((user) => res.status(200).send(user))
+    .catch(() => res.status(404).send({ error: 'Пользователь не найден' }));
+};
+
+export const updateUserAvatar = async (req: Request, res: Response) => {
+  // @ts-ignore
+  const id = req.user._id;
+
+  await User.findByIdAndUpdate(id, {
+    avatar: req.body.avatar,
+  }, {
+    new: true,
+  }).exec()
+    .then((user) => res.status(200).send(user))
+    .catch(() => res.status(404).send({ error: 'Пользователь не найден' }));
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,26 +1,18 @@
-import { Router, Request, Response } from 'express';
-import User from '../models/user';
-import createUser from '../controllers/users';
+import { Router } from 'express';
+import {
+  createUser, getUserById, getUsers, updateUserAvatar, updateUserProfile,
+} from '../controllers/users';
 
-// [{"_id":"635e4b11541f2e456422e8a1","name":"Bill","about":"bill@initech.com","avatar":"https://i.imgur.com/dM7Thhn.png","__v":0},{"_id":"635e4c46b3b6721a4e6a7c39","name":"Bill","about":"bill@initech.com","avatar":"https://i.imgur.com/dM7Thhn.png","__v":0},{"_id":"635e4cabdcdc287d424a9855","name":"Bill","about":"bill@initech.com","avatar":"https://i.imgur.com/dM7Thhn.png","__v":0},{"_id":"635e524e989ff3f476d10c21","name":"Bill","about":"bill@initech.com","avatar":"https://i.imgur.com/dM7Thhn.png","__v":0}]
 const router = Router();
 
-router.get('/users', async (req: Request, res: Response) => {
-  await User.find({})
-    .then((users) => {
-      res.status(200).send(users);
-    })
-    .catch((err) => console.log(err));
-});
+router.get('/users', getUsers);
 
-router.get('/users/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  console.log(req.params.id);
-  await User.findById(id).exec()
-    .then((user) => res.status(200).send(user))
-    .catch(() => res.status(404).send({ error: 'Пользователь не найден' }));
-});
+router.get('/users/:id', getUserById);
 
 router.post('/users', createUser);
+
+router.patch('/users/me', updateUserProfile);
+
+router.patch('/users/me/avatar', updateUserAvatar);
 
 export default router;
