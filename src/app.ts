04323847b53dec524a8f2bd702f97f14d0499fbd
Elena Soloveva <seaver@yandex.ru>
2022-10-30 09:48:34 +0200

    fix: disable no-console rule for eslint

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 
 // Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
-
+const { PORT = 4000 } = process.env;
+const uri = 'mongodb+srv://seaver:50f6Y2YYrbkmpQcj@cluster0.qf2ysmc.mongodb.net/?retryWrites=true&w=majority';
 const app = express();
+mongoose
+  .connect(uri)
+  .then(() => app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  }))
+  .catch((err) => console.log(err));
 
-
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
   res.status(200).send('Mesto App');
-})
+});
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
