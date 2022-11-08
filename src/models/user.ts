347ff1ce64b1f112a6b3ab0921fa76878a070648
Elena Soloveva <seaver@yandex.ru>
2022-11-08 13:09:43 +0200

    feat: make password not to be in returned to front user information

--- src/models/user.ts
+++ src/models/user.ts
@@ -24,6 +24,7 @@ const userSchema = new Schema<IUser, IUserModel>({
     type: String,
     required: true,
     minLength: [6, 'Пароль должен быть не менее 6 символов, получено {VALUE}'],
+    select: false,
   },
   name: {
     type: String,
