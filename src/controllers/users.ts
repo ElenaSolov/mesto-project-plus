b1b449d4593bc8ef1f6d1779be00b69c05b78ca4
Elena Soloveva <seaver@yandex.ru>
2022-11-17 09:11:48 +0200

    fix: delete Bearer word from token

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -103,7 +103,7 @@ export const login = (req: Request, res: Response, next: NextFunction) => {
       if (!user) {
         next(messageNotValidEmailOrPassword);
       } else {
-        const token = `Bearer: ${jwt.sign({ _id: user._id }, jwtsecret, { expiresIn: ONE_WEEK })}`;
+        const token = jwt.sign({ _id: user._id }, jwtsecret, { expiresIn: ONE_WEEK });
         res.cookie('token', token, {
           httpOnly: true,
           maxAge: ONE_WEEK_IN_MS,
